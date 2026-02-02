6b049140ce1ba15d72b586f2c21de814cdf71b44
Anishj2029 <anishjoshi2029@gmail.com>
2026-02-02 21:55:24 +0530

    Reverse digits

---
+++ Reverse digits.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int findSUM(int n){
+    
+    int sum = 0;
+    
+    while(n != 0){
+    int nt = n % 10;
+    sum = sum* 10 + nt;
+    
+    n = n / 10;
+    }
+    return sum;
+    
+}
+
+int main(){
+    int n;
+    cout<<"Enter any number : ";
+    cin>>n;
+    
+    cout<<findSUM(n);
+}
